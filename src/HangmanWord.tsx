835beb40e9b8b26969a495f893dd16d14b3a63de
Joseph Thompson <joe.thompson838@gmail.com>
2024-05-14 09:21:49 -0400

    feat: finish up hangman project

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,38 @@
-export function HangmanWord() {
-    
+type HangmanWordProps = {
+  guessedLetters: string[];
+  wordToGuess: string;
+};
+
+export function HangmanWord({ guessedLetters, wordToGuess }: HangmanWordProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter: string, index: number) => (
+        <span
+          key={index}
+          style={{
+            borderBottom: ".1em solid black",
+          }}
+        >
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
