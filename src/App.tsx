b8ee6266c321cc9bad3ce4337d9d738a433b0c2c
Joseph Thompson <joe.thompson838@gmail.com>
2024-05-14 08:07:28 -0400

    feat: add basic react app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import words from './wordList.json';
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => words[Math.floor(Math.random() * words.length)])
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  return (
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{ fontSize: "2rem", textAlign: "center"}}>
+        Lose
+        Win
+      </div>
+    </div>
+  )
+}
+
+export default App
